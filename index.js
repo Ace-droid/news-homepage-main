72c1c75989bf38d6ef3649112164736c429917a5
henry onoja <99752876+Ace-droid@users.noreply.github.com>
2023-08-11 10:20:36 +0100

    stopped the page from srolling when  menu is open

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const menuButton = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-
-
 const body = document.querySelector("body");
 
 
 
+let isNavOpen = false;
       menuButton.addEventListener("click", () => {
+        isNavOpen = !isNavOpen;
+        if (isNavOpen) {
+          body.style.overflowY = "auto";
+        } else {
+          body.style.overflowY = "hidden";
+        }
             menuButton.classList.toggle("is-active");
             navLinks.classList.toggle("is-active");
-            body.classList.toggle("blur");
-            body.classList.toggle("fixed");
       })
 
 
